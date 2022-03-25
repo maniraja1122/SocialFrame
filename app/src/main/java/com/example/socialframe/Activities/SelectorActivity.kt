0e6c592e259b15ac4166e8dcf031efc2611eb7ad
maniraja1122 <araja.bscs20seecs@seecs.edu.pk>
2022-03-25 18:42:47 +0500

    First Update

---
+++ app/src/main/java/com/example/socialframe/Activities/SelectorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialframe.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialframe.R
+
+class SelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selector)
+    }
+}
